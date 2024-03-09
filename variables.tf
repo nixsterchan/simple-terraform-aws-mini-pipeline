18eec24ba92f12e10637e34d3071ad03f156ab0a
nigelchan <nigelchan95@gmail.com>
2024-03-10 04:30:49 +0800

    updated variables and shifted them to tfvars instead

--- variables.tf
+++ variables.tf
@@ -1,24 +1,18 @@
 variable "the_processing_lambda_name" {
   type = string
-  default = "lambda-processing-module"
 }
 variable "region" {
   type = string
-  default = "ap-southeast-1"
 }
 variable "ingestion_bucket_name" {
   type = string
-  default = "mini-pipe-ingestion"
 }
 variable "processed_output_bucket_name" {
   type = string
-  default = "mini-pipe-processed-outputs"
 }
 variable "proc_lambda_sqs_name" {
   type = string
-  default = "queue-lambda-processing-module"
 }
 variable "proc_lambda_dlq_name" {
   type = string
-  default = "dlq-lambda-processing-module"
 }
