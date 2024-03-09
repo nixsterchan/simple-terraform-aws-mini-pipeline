467cec47e3026c5c796c0ea4f86c1a867bb5c03e
nigelchan <nigelchan95@gmail.com>
2024-03-10 02:58:36 +0800

    updated variables to include bucket and sqs queues

--- variables.tf
+++ variables.tf
@@ -6,3 +6,19 @@ variable "region" {
   type = string
   default = "ap-southeast-1"
 }
+variable "ingestion_bucket_name" {
+  type = string
+  default = "mini-pipe-ingestion"
+}
+variable "processed_output_bucket_name" {
+  type = string
+  default = "mini-pipe-processed-outputs"
+}
+variable "proc_lambda_sqs_name" {
+  type = string
+  default = "queue-lambda-processing-module"
+}
+variable "proc_lambda_dlq_name" {
+  type = string
+  default = "dlq-lambda-processing-module"
+}
