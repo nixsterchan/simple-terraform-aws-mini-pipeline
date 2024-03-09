c8c3af3c9803a1f83e71dd4b19ea222d06fa85f6
nigelchan <nigelchan95@gmail.com>
2024-03-10 02:59:01 +0800

    updated main.tf to include the creation of queues as well as setting up of s3 to send event notification to the queue

--- main.tf
+++ main.tf
@@ -37,10 +37,56 @@ EOF
 
 module "ingestion_bucket" {
   source = "./modules/s3"
-  bucket_name = "mini-pipe-ingestion"
+  bucket_name = var.ingestion_bucket_name
 }
 
 module "processed_output_bucket" {
   source = "./modules/s3"
-  bucket_name = "mini-pipe-processed-outputs"
+  bucket_name = var.processed_output_bucket_name
+}
+
+module "queue_for_processing_lambda" {
+  source = "./modules/sqs"
+  queue_name = var.proc_lambda_sqs_name
+  delay_seconds = 0
+  max_message_size = 2048
+  message_retention_seconds = 345600
+  visibility_timeout_seconds = 300
+  max_receive_count = 3
+  sqs_policy_document = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "SQSPolicy",
+  "Statement": [
+    {
+      "Sid": "AllowReceiveMessageFromS3Bucket",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "sqs:SendMessage",
+      "Resource": "arn:aws:sqs:${var.region}:*:${var.proc_lambda_sqs_name}",
+      "Condition": {
+        "ArnEquals": {
+          "aws:SourceArn": "${module.ingestion_bucket.s3_bucket_arn}"
+        }
+      }
+    }
+  ]
+}
+EOF
+  create_dlq = true
+  dead_letter_queue_name = var.proc_lambda_dlq_name
+}
+
+# Setup S3 to send notifications to the queue
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.ingestion_bucket.bucket_id
+
+  queue {
+    queue_arn = module.queue_for_processing_lambda.main_queue_arn
+    events = ["s3:ObjectCreated:*"]
+    filter_prefix = ""
+    filter_suffix = ""
+  }
 }
