82cf047a59b680e1492aa5c832f53508ab49a6fe
nigelchan <nigelchan95@gmail.com>
2024-03-10 04:08:05 +0800

    updated s3 module outputs to include s3 bucket it

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -2,3 +2,7 @@ output "s3_bucket_arn" {
   description = "The Amazon Resource Name (ARN) of the created S3 bucket."
   value       = aws_s3_bucket.s3_bucket.arn
 }
+output "s3_bucket_id" {
+  description = "The Amazon Resource Name (ARN) of the created S3 bucket."
+  value       = aws_s3_bucket.s3_bucket.id
+}
